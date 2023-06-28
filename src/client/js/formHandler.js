c8661d783c0d949ff64a83848522f51f0a49494e
LOLO <luodairou@Laurens-Mac-mini.local>
2023-06-28 17:39:39 +1200

    all js function work well

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,11 +5,15 @@ function handleSubmit(event) {
   let formText = document.getElementById("name").value;
   Client.checkForName(formText);
 
+  console.log(formText);
   console.log("::: Form Submitted :::");
+
   fetch("http://localhost:8081/test")
-    .then((res) => res.json())
-    .then(function (res) {
-      document.getElementById("results").innerHTML = res.message;
+    .then((res) => {
+      return res.json();
+    })
+    .then(function (data) {
+      document.getElementById("results").innerHTML = data.message;
     });
 }
 
